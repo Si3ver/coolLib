b6d6fa69006f11d8232490769eab8696062b869e
Si3ver <izhouwl@163.com>
2019-01-20 22:35:15 +0800

    init a vue app

---
+++ vue-demo/README.md
@@ -0,0 +1,5 @@
+# vue-demo
+
+Vue实现一个todolist
+
+![待办事项网页截图](README/WechatIMG13.png)
---
+++ vue-demo/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Todolist />
+  </div>
+</template>
+
+<script>
+import Todolist from './components/Todolist'
+
+export default {
+  name: 'App',
+  components: {
+    Todolist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
